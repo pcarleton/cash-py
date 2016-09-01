1c718a8284d04ce5f0d22a73689817bbe2a444ec
Paul Carleton <paulcarletonjr@gmail.com>
2016-08-31 20:39:08 -0700

    Initial commit

---
+++ slack.py
@@ -0,0 +1,85 @@
+from slackclient import SlackClient
+
+BOT_NAME = 'jarvis'
+
+token = 'xoxb-68771372545-IrK1bV4NwGVUPeqVrxry49Tm'
+
+slack_client = SlackClient(token)
+
+BOT_ID = u'U20NPAYG1'
+direct_channel = u'D20NK622Z'
+
+import time
+import sys
+import logging
+
+import spending
+
+
+
+logger = logging.getLogger("main")
+
+def serve(delay=1):
+    if slack_client.rtm_connect():
+        logger.info("Connected, running.")
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+
+            time.sleep(delay)
+    else:
+        logger.warn('No connection')
+
+sheet_link = u"https://docs.google.com/spreadsheets/d/1s_imsHhZuC-iDX3R0I0s2rN2uFjaWB6UXrenjf7rnVg/edit#gid=0"
+
+def handle_command(command, channel):
+    def _respond(msg):
+        slack_client.api_call("chat.postMessage", channel=channel,
+                             text=msg,
+                             as_user=True)
+
+    if command.startswith("balance"):
+        response = "Don't have that info."
+        _respond(response)
+
+    elif 'sheet' in command:
+        _respond(sheet_link)
+    elif 'update' in command:
+        _respond("Updating spending ...")
+        spending.update_spending()
+        _respond("Sheet updated: %s" % sheet_link)
+    else:
+        response = "Unrecognized command."
+        slack_client.api_call("chat.postMessage", channel=channel,
+                             text=response,
+                             as_user=True)
+
+
+def parse_slack_output(slack_rtm_output):
+    """
+        The Slack Real Time Messaging API is an events firehose.
+        this parsing function returns None unless a message is
+        directed at the Bot, based on its ID.
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            logger.debug(output)
+#             if 'channel' in output and output['channel'] == direct_channel:
+#                 return output['text'], output['channel']
+            if (output and 'text' in output and # and AT_BOT in output['text'] and
+                'channel' in output and output['channel'] == direct_channel and
+               'user' in output and output['user'] != BOT_ID):
+                # return text after the @ mention, whitespace removed
+                return output['text'].strip().lower(), output['channel']
+    return None, None
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(name)s %(asctime)s - %(levelname)s - %(message)s",
+        stream=sys.stdout)
+
+    serve()
---
+++ spending.py
@@ -0,0 +1,103 @@
+import pandas as pd
+
+import gsheets
+from plaid import Client
+import logging
+from secrets import CLIENT_ID, CLIENT_SECRET, PAUL_TOKEN, TAYLOR_TOKEN, NICKNAMES
+
+
+Client.config({
+    'url': 'https://tartan.plaid.com'
+})
+
+
+logger = logging.getLogger("main")
+
+
+
+SPENDING_COLS = ['_id', 'account', 'date', 'name', 'amount', 'categories']
+
+def fetch_transactions(token, start_date):
+    client = Client(client_id=CLIENT_ID,
+                secret=CLIENT_SECRET,
+               access_token=token)
+
+    response = client.connect_get(opts={'gte': start_date})
+    trans = response.json()
+    df = pd.DataFrame(trans['transactions'])
+
+    return df
+
+
+def get_new_spending():
+    start_date = '2016-02-01'
+    logging.info("Fetching Paul...")
+    paul_spending = fetch_transactions(PAUL_TOKEN, start_date)
+    logging.info("Fetching Taylor...")
+    taylor_spending = fetch_transactions(TAYLOR_TOKEN, start_date)
+
+    all_df = pd.concat([paul_spending, taylor_spending]).sort_values(by='date', ascending=False)
+
+
+    all_df['account'] = all_df._account.apply(lambda account_id: NICKNAMES[account_id])
+    # Categories is a list which can't be sent as JSON to a google sheet
+    all_df['categories'] = all_df.category.apply(lambda cs: ", ".join(cs) if type(cs) == list else '')
+    spending_columns = ['_id', 'account', 'date', 'name', 'amount', 'categories']
+
+    return all_df[SPENDING_COLS]
+
+def get_current_spending(spreadsheet, sheet_name):
+    cur_df = spreadsheet.get_sheet(sheet_name)
+
+    return cur_df
+
+def get_budget_spreadsheet():
+    spreadsheet_name = "Summer 2016 Budget"
+    sheet_name = "all-together"
+    creds_dir = ".pcreds"
+
+    auth_options = gsheets.auth.AuthOptions(credentials_dir=creds_dir)
+    spreadsheet = gsheets.get_spreadsheet(spreadsheet_name, options=auth_options)
+
+    return spreadsheet
+
+def update_spending():
+    new_df = get_new_spending()
+
+    ss = get_budget_spreadsheet()
+    logging.info("Getting current spending...")
+    old_df = get_current_spending(ss, "all-together")
+
+    logging.info("Merging old and new.")
+    both_df = pd.concat([old_df, new_df])
+
+    dedup_df = both_df.drop_duplicates("_id")
+
+    excluded = old_df[['_id', 'exclude']]
+
+    spend_df = dedup_df[SPENDING_COLS].merge(excluded, on='_id', how='left')
+
+    logging.info("Saving spreadsheet...")
+    sheet_name = "all-together"
+    ss.save_sheet(sheet_name, spend_df)
+
+    logging.info("Success!")
+
+
+def new_spending_df():
+    new_df = get_new_spending()
+
+    ss = get_budget_spreadsheet()
+    logging.info("Getting current spending...")
+    old_df = get_current_spending(ss, "all-together")
+
+    logging.info("Merging old and new.")
+    both_df = pd.concat([old_df, new_df])
+
+    dedup_df = both_df.drop_duplicates("_id")
+
+    excluded = old_df[['_id', 'exclude']]
+
+    spend_df = dedup_df[SPENDING_COLS].merge(excluded, on='_id', how='left')
+
+    return spend_df
